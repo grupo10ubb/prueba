15f13b5b516e3229a14364b582ca318bbcaa728d
Guillermo <Guillermo@10.1.11.122>
2014-04-07 17:31:45 -0300

    segundo commit

--- TallerGit/src/cl/taller/git/Main.java
+++ TallerGit/src/cl/taller/git/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Mi primer commit");
+System.out.println("Mi segundo commit");
 	}
 
 }
